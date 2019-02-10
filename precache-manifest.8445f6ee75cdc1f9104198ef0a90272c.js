99fe2cea419980bdda1fd02812c83bbd5b07f204
DBjelovuk <drazen.bjelovuk@gmail.com>
2019-02-09 19:12:48 -0500

    Initial commit

---
+++ precache-manifest.8445f6ee75cdc1f9104198ef0a90272c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2411d5f09fe3111e2708664d37124473",
+    "url": "/static/media/logo.2411d5f0.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c06cc47d1519e8bbfd4b",
+    "url": "/static/js/main.c06cc47d.chunk.js"
+  },
+  {
+    "revision": "dc8590dd1f5839c1c11f",
+    "url": "/static/js/1.dc8590dd.chunk.js"
+  },
+  {
+    "revision": "c06cc47d1519e8bbfd4b",
+    "url": "/static/css/main.d0c2ce87.chunk.css"
+  },
+  {
+    "revision": "9f8751d897ba33a9805e0b837d5768cf",
+    "url": "/index.html"
+  }
+];
